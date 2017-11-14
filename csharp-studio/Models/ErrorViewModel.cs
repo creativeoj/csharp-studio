4440da887126b81fd0f2ebd65f8bdbffd5a354ff
OJ SHin <tellmejin@gmail.com>
2017-11-14 01:41:18 -0600

    Add project files.

---
+++ csharp-studio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace csharp_studio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
